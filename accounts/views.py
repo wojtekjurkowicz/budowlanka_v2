f4c4f750dee9cd8d83f5f3f6aff67396b62c97f8
wojtekjurkowicz <wojtek.jurkowicz@gmail.com>
2024-06-11 14:09:13 +0200

    dodane powiadomienia

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
 
-def register(request):
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Pomyślnie zalogowano.')
+                return redirect('mainapp:index')
+            else:
+                messages.error(request, 'Nieprawidłowe dane logowania.')
+        else:
+            messages.error(request, 'Nieprawidłowe dane logowania.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def register_view(request):
     # register a new user
     if request.method != 'POST':
         # Display blank registration form
@@ -11,11 +32,11 @@ def register(request):
     else:
         # Process completed form
         form = UserCreationForm(data=request.POST)
-
         if form.is_valid():
             new_user = form.save()
             # Log the user in and then redirect to home page
             login(request, new_user)
+            messages.success(request, 'Rejestracja zakończona sukcesem.')
             return redirect('mainapp:index')
 
     # Display a blank or invalid form
